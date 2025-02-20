c6080bb59d30181da52c27b8695910c4b541cc01
Luciano Costa <lucianocosta21@hotmail.com>
2025-02-20 17:46:20 -0300

    user-service

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,19 +1,12 @@
-import bcrypt from "bcrypt";
-import { UserModel } from "../models/user-models";
-import jwt from "jsonwebtoken";
+import { UserModel } from "../models/user-models"
 
-export class AuthService {
-  async login(email:string, password: string){
-    const userModel = await UserModel.findByEmail(email);
-    if(userModel && bcrypt.compareSync(password, userModel.password)){
-      return jwt.sign({id: userModel.id, email: userModel.email},
-        "123456",{
-          expiresIn: "1h",
-        });
-    }else {
-      throw new InvalidCredentialsError();
-    }
+export class UserService{
+
+  async findById(userId: number){
+    return UserModel.findById(userId);
+  }
+
+  async findByEmail(email: string){
+    return UserModel.findByEmail(email);
   }
 }
-
-export class InvalidCredentialsError extends Error{}
