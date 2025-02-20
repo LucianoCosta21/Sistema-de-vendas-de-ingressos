f73948003c885d4ae73394716f971df606788d77
Luciano Costa <lucianocosta21@hotmail.com>
2025-02-20 17:19:03 -0300

    partner-service

--- src/services/partner-service.ts
+++ src/services/partner-service.ts
@@ -1,19 +1,50 @@
-import bcrypt from "bcrypt";
+import { Database } from "../database";
+import { PartnerModel } from "../models/partner-model";
 import { UserModel } from "../models/user-models";
-import jwt from "jsonwebtoken";
 
-export class AuthService {
-  async login(email:string, password: string){
-    const userModel = await UserModel.findByEmail(email);
-    if(userModel && bcrypt.compareSync(password, userModel.password)){
-      return jwt.sign({id: userModel.id, email: userModel.email},
-        "123456",{
-          expiresIn: "1h",
-        });
-    }else {
-      throw new InvalidCredentialsError();
+export class PartnerService{
+  async register(data: {
+    name: string;
+    email: string;
+    password: string;
+    company_name: string;
+  }){
+    
+    const {name, email, password, company_name} = data
+    const connection = await Database.getInstance().getConnection();
+
+    try {
+      await connection.beginTransaction();
+
+      const user = await UserModel.create(
+              {
+                name,
+                email,
+                password,
+              },
+              {connection}
+            );
+      const partner = await PartnerModel.create(
+        {
+          company_name,
+          user_id: user.id,
+        },
+        { connection }
+      );
+        await connection.commit();
+        return{
+          id: partner.id,
+          name,
+          user_id: user.id,
+          company_name,
+          created_at: partner.created_at,
+        }       
+    } catch (e) {
+      await connection.rollback();
+      throw e;  
     }
   }
+  async findByUserId(userId:number){
+    return PartnerModel.findById(userId);
+  }
 }
-
-export class InvalidCredentialsError extends Error{}
